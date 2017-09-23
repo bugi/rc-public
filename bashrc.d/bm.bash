0b3c99975f54d420abd274b2938f0ab640668fa3
Brian Grossman <brian.grossman+github@gmail.com>
2017-09-23 12:29:02 -0600

    change to using associative arrays, so now requiring bash version >= 4

--- bashrc.d/bm.bash
+++ bashrc.d/bm.bash
@@ -2,16 +2,25 @@
 # implements simple bookmark-style friend for pushd/popd
 #
 
+declare -A _BM_=()
+unset -f go
+
 # bm - set bookmark for current directory
 # arg is boomark name
 function bm {
-	eval _bm_$1="`pwd`"
+	local space=' '
+	if [[ $1 =~ $space ]]
+	then
+		echo "spaces are not allowed in bookmark names" 1>&2
+	else
+		_BM_["$1"]="$(pwd)"
+	fi
 	}
 
 # go - go to the directory referenced by the given bookmark
 # arg is boomark name
-function go {
-	eval cd \$_bm_$1
+function wend {
+	cd "${_BM_["$1"]}"
 	}
 
 # wend - go to the directory referenced by the given bookmark
@@ -20,62 +29,116 @@ function go {
 # second and further args are optional
 # "wend" because that is (or was) the present tense of "went"
 # "gan" because the old english "gān"
-function wend {
-	# The simpler, but safer version:
-	#	eval cd \$_bm_$1
+function gan {
+	# For the simpler, but safer version see "wend".
 	#
-	# the nested evals allows a second level of substitution, so you could have a bm like
-	#	_bm_foo=/foo/\${2}/bar
+	# For gan, the nested evals allows a second level of substitution,
+	# so you could have a bm like
+	#	_BM_[foo]=/foo/\${1}/bar
 	# and if you said
-	#	go foo baz
+	#	gan foo baz
 	# you would wind up at
 	#	/foo/baz/bar
 	#
-	# Be careful though.  That "\${2}" could've been "\$(hostname)" or "\$(rm -rf /)".
+	# Be careful though.  There's bound to be a way to
+	# exploit that the "\${1}" could've been
+	# "\$(hostname)" or "\$(rm -rf /)".
 	#
-	eval cd $(eval "echo \$_bm_$1")
-	}
-function gan {
-	wend "$@"
+	local k="$1" ; shift
+	local v="$( eval echo "${_BM_["$k"]}" )"
+	cd "$v"
 	}
 
 # lsbm - list the bookmarks
 # optional arg restricts the beginning of the list
 # so, 'lsbm h' would list home, but not foo
-function lsbm {
-	set | grep ^_bm_$1 | sed 's/^_bm_//;s/=/	/'
+function lsbm_keys {
+	local k
+	for k in "${!_BM_[@]}"
+	do
+		echo "$k"
+	done |grep -v ' '
 	}
-alias ldirs=lsbm
-alias lbm=lsbm
+function lsbm_keys_filter {
+	if [ -z "$1" ]
+	then
+		lsbm_keys
+	else
+		lsbm_keys | grep "^$1"
+	fi
+	}
+function lsbm {
+	for k in $( lsbm_keys_filter "$1" )
+	do
+		if [ -n "$k" ]
+		then
+			v="${_BM_["$k"]}"
+			echo "$k	$v"
+		fi
+	done
+	}
 
 # rmbm - remove the referenced bookmark
 # arg is boomark name
 function rmbm {
-	unset _bm_$1
+	unset _BM_["$1"]
 	}
 
 # savebm - save a bookmark set
-# first arg is name of bookmark set
-# second (optional) arg is like the optional arg of lsbm
+#
+# The first arg is name of bookmark set, defaulting to "default".
+# That first arg can be "-" which means STDOUT.
+#
+# The second (optional) arg is like the optional arg of lsbm
+#
+# However, I suggest maintaining most of your bookmark files manually.
+# That way you can make use of comments, loops and whatnot.
 function savebm {
 	local bum="$1" ; shift
-	local nm="$1" ; shift
-	if [ -n "$bash_RC_private" ]
+	if [ -z "$bum" ]
 	then
-		set | grep "^_bm_$nm" > "$bash_RC_private/bm.bash.list.$bum"
-	else
-		set | grep "^_bm_$nm" > "$HOME/.bm.bash.list.$bum"
+		bum=default
 	fi
+	local nm="$1" ; shift
+	local fn="$bash_RC_private/bm.bash.list.$bum"
+	if [ "$bum" = - ]
+	then
+		fn=/dev/stdout
+	elif [ -n "$bash_RC_private" ]
+	then
+		fn="$HOME/.bm.bash.list.$bum"
+	elif [ -w "$bash_RC_private/bm.bash.list.$bum" ]
+	then
+		fn="$bash_RC_private/bm.bash.list.$bum"
+	elif [ -w "$bash_RC_public/bm.bash.list.$bum" ]
+	then
+		fn="$bash_RC_public/bm.bash.list.$bum"
+	fi
+	local k
+	for k in $( lsbm_keys_filter "$nm" )
+	do
+		v="${_BM_["$k"]}"
+		echo "_BM_['$k']='$v'"
+	done > "$fn"
 	}
 
 # loadbm - load a bookmark set, as saved by savebm
 # arg is the name of the bookmark set
 function loadbm {
 	local bum="$1" ; shift
+	if [ -z "$bum" ]
+	then
+		bum=default
+	fi
+	if [ -r "$bash_RC_public/bm.bash.list.$bum" ]
+	then
+		. "$bash_RC_public/bm.bash.list.$bum"
+	fi
 	if [ -r "$bash_RC_private/bm.bash.list.$bum" ]
 	then
 		. "$bash_RC_private/bm.bash.list.$bum"
-	elif [ -r "$HOME/.bm.bash.list.$bum" ]
+	fi
+	if [ -r "$HOME/.bm.bash.list.$bum" ]
 	then
 		. "$HOME/.bm.bash.list.$bum"
 	fi
